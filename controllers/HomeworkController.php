cac9e98f16849bd96236fa3d63d3e31bbd1b57ea
Reininger Andreas <andreas.reininger@it.htl-hl.ac.at>
2026-02-27 10:22:53 +0100

    assignments dont work

--- controllers/HomeworkController.php
+++ controllers/HomeworkController.php
@@ -32,22 +32,13 @@ class HomeworkController extends Controller
 
     public function actionIndex()
     {
-
-        $activeHomeworks = Homework::find()
-            ->where(['!=', 'status', 'Finished'])
-            ->orWhere(['status' => null])
+        $homeworks = Homework::find()
+            ->where(['user_id' => Yii::$app->user->id])
             ->orderBy(['due_date' => SORT_ASC])
             ->all();
 
-
-        $finishedHomeworks = Homework::find()
-            ->where(['status' => 'Finished'])
-            ->orderBy(['due_date' => SORT_DESC])
-            ->all();
-
         return $this->render('index', [
-            'activeHomeworks' => $activeHomeworks,
-            'finishedHomeworks' => $finishedHomeworks,
+            'homeworks' => $homeworks,
         ]);
     }
 
@@ -55,19 +46,11 @@ class HomeworkController extends Controller
     {
         $model = new Homework();
 
-        if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
+        if ($model->load(Yii::$app->request->post())) {
+            $model->user_id = Yii::$app->user->id;
+            if ($model->save()) {
                 return $this->redirect(['index']);
             }
-        } else {
-            $model->loadDefaultValues();
-        }
-
-        // Use renderAjax instead of render
-        if (Yii::$app->request->isAjax) {
-            return $this->renderAjax('create', [
-                'model' => $model,
-            ]);
         }
 
         return $this->render('create', [
@@ -90,31 +73,13 @@ class HomeworkController extends Controller
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
 
-        // Wir suchen direkt in der Teachers-Tabelle nach der subject_id
         $teachers = Teachers::find()
-            ->where(['subject_id' => $id])
-            ->andWhere(['status' => 1]) // Optional: nur aktive Lehrer anzeigen
+            ->innerJoin('Subject_Has_Teacher sht', 'sht.teacher_id = Teachers.id')
+            ->where(['sht.subject_id' => $id])
             ->all();
 
         return ArrayHelper::map($teachers, 'id', function($t){
             return $t->firstname . ' ' . $t->lastname;
         });
-    }
-
-    public function actionToggleStatus($id)
-    {
-        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        // We allow both POST and GET for this test to ensure it works
-        $model = Homework::findOne($id);
-
-        if ($model) {
-            $model->status = ($model->status === 'Finished') ? 'Open' : 'Finished';
-            if ($model->save()) {
-                return ['success' => true, 'newStatus' => $model->status];
-            }
-        }
-
-        return ['success' => false, 'error' => 'Model not found or not saved'];
     }
 }
